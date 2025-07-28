ff1113dda9daa3e463b9d3f349f8724ad64916ed
John Reese <j.charles.reese@gmail.com>
2025-07-28 10:49:07 -0700

    Adding feature_eng helper method

--- forecastos/feature.py
+++ forecastos/feature.py
@@ -1,5 +1,6 @@
 from forecastos.utils.readable import Readable
 import pandas as pd
+import numpy as np
 import os
 
 
@@ -76,3 +77,118 @@ class Feature(Readable):
 
     def __str__(self):
         return f"Feature_{self.uuid}_{self.name}"
+    
+    @classmethod
+    def create_feature_df(cls, config={}, base_df=None):
+        df = base_df.copy()
+        print("Sorting base df.")
+        df = df.sort_values(["datetime", "id"])
+
+        # Get raw features
+        for ft_name, ft in config.get('features', []).items():
+            print(f"Getting {ft_name}.")
+            tmp_ft_df = cls.get(ft["uuid"]).get_df().rename(columns={"value": ft_name})
+            
+            print(f"Merging {ft_name}.")
+            tmp_ft_df = tmp_ft_df.sort_values(["datetime", "id"])
+            df = pd.merge_asof(df, tmp_ft_df, on="datetime", by="id", direction='backward')
+        
+        # D Calculate raw derived features
+        df = cls.apply_feature_engineering_logic(df, config, "features_derived", logic_dict_key='formula', calculate_with="raw")
+
+        # Run adjustments on all (excl. normalized derived features)
+        df = cls.apply_feature_engineering_logic(df, config, "features", logic_dict_key='adjustments')
+        df = cls.apply_feature_engineering_logic(df, config, "features_derived", logic_dict_key='adjustments', calculate_with="raw")
+
+        # Run normalization on all (excl. normalized derived features)
+        df = cls.apply_feature_engineering_logic(df, config, "features", logic_dict_key='normalization', global_logic_dict_key='feature_normalization')
+        df = cls.apply_feature_engineering_logic(df, config, "features_derived", logic_dict_key='normalization', calculate_with="raw", global_logic_dict_key='feature_normalization')
+
+        # Run post-norm adjustments on all (excl. normalized derived features)
+        df = cls.apply_feature_engineering_logic(df, config, "features", logic_dict_key='adjustments_post_normalization')
+        df = cls.apply_feature_engineering_logic(df, config, "features_derived", logic_dict_key='adjustments_post_normalization', calculate_with="raw")
+
+        # D Calculate normalized derived features
+        df = cls.apply_feature_engineering_logic(df, config, "features_derived", logic_dict_key='formula', calculate_with="normalized")
+
+        return df
+    
+    @classmethod
+    def apply_feature_engineering_logic(cls, df, config, features_key, logic_dict_key='formula', calculate_with=None, global_logic_dict_key=None):
+        for ft_name, ft in ((k, v) for k, v in config.get(features_key, {}).items() if not calculate_with or v.get("calculate_with") == calculate_with):
+            for formula_name, arg_li in ft.get(logic_dict_key, config.get(global_logic_dict_key, {})).items(): 
+                df = cls.apply_formula(df, ft_name, formula_name, arg_li)
+
+        return df
+    
+    @classmethod
+    def apply_formula(cls, df, ft_name, formula_name, arg_li):
+        def apply_mean(df, ft_name, arg_li):
+            print(f"Applying mean for {ft_name} feature using {arg_li}.")
+            df[ft_name] = df[arg_li].mean(axis=1)
+
+            return df
+        
+        def apply_subtract(df, ft_name, arg_li):
+            print(f"Applying subtract for {ft_name} feature using {arg_li}.")
+            df[ft_name] = df[arg_li[0]] - df[arg_li[1]]
+            
+            return df
+        
+        def apply_neg_to_max(df, ft_name, arg_li):
+            print(f"Applying neg_to_max for {ft_name} feature using {arg_li}.")
+            group_max = df.groupby(arg_li)[ft_name].transform('max')
+            df[ft_name] = np.where(df[ft_name] < 0, group_max, df[ft_name])
+        
+            return df
+
+        def apply_sign_flip(df, ft_name, arg_li):
+            print(f"Applying sign_flip for {ft_name} feature.")
+            df[ft_name] = df[ft_name] * -1
+        
+            return df
+        
+        def apply_winsorize(df, ft_name, arg_li):
+            lower_q = arg_li[0]
+            upper_q = arg_li[1]
+            group_by = arg_li[2]
+            print(f"Applying winsorize for {ft_name} feature using {lower_q} and {upper_q}, grouped by {group_by}.")
+
+            df[ft_name] = (
+                df.groupby(group_by)[ft_name]
+                .transform(lambda x: x.clip(lower=x.quantile(lower_q), upper=x.quantile(upper_q)))
+            )
+
+            return df
+        
+        def apply_standardize(df, ft_name, arg_li):
+            print(f"Applying standardize for {ft_name} feature using {arg_li}.")
+            df[ft_name] = df.groupby(arg_li)[ft_name].transform(
+                lambda x: (x - x.mean()) / x.std(ddof=0)
+            )
+
+            return df
+        
+        def apply_zero_fill(df, ft_name, arg_li):
+            print(f"Applying zero_fill for {ft_name} feature.")
+            df[ft_name] = df[ft_name].fillna(0)
+
+            return df
+
+
+        match formula_name:
+            case "mean":
+                return apply_mean(df, ft_name, arg_li)
+            case "subtract":
+                return apply_subtract(df, ft_name, arg_li)
+            case "neg_to_max":
+                return apply_neg_to_max(df, ft_name, arg_li)
+            case "sign_flip": 
+                return apply_sign_flip(df, ft_name, arg_li)
+            case "winsorize":
+                return apply_winsorize(df, ft_name, arg_li)
+            case "standardize":
+                return apply_standardize(df, ft_name, arg_li)
+            case "zero_fill":
+                return apply_zero_fill(df, ft_name, arg_li)
+
