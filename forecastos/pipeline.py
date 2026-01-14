27fa6800fe9ee37611947df70a658a2b345cb712
ksackvil <sackvillehiik@gmail.com>
2026-01-14 15:59:06 -0800

    add pipeline helper

---
+++ forecastos/pipeline.py
@@ -0,0 +1,19 @@
+from forecastos.utils.readable import Readable
+from forecastos.utils.feature_engineering_mixin import FeatureEngineeringMixin
+import pandas as pd
+import io
+
+
+class Pipeline(Readable, FeatureEngineeringMixin):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def get_df(cls, pipeline_id, run_id):
+        res = cls.get_request(
+            path=f"/pipelines/{pipeline_id}/runs/{run_id}", use_team_key=True)
+        if not res.ok:
+            print(res)
+            return False
+
+        return pd.read_csv(io.StringIO(res.text))
